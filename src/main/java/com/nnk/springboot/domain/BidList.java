8982e33cfd86387c00b52245e71012b430e14323
Mickael Haye <mickael.haye@gmail.com>
2023-08-17 09:51:45 +0200

    entity bidList

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,16 +1,79 @@
 package com.nnk.springboot.domain;
 
-//retirer au début à valider
-//import org.springframework.beans.factory.annotation.Required;
-
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import java.sql.Date;
 import java.sql.Timestamp;
 
+//retirer au début à valider
+//import org.springframework.beans.factory.annotation.Required;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "bidlist")
+@DynamicUpdate
+@Table(name = "bid_list")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "BidListId")
+	private int BidListId;
+	@Column(name = "account")
+	private String account;
+	@Column(name = "type")
+	private String type;
+	@Column(name = "bidQuantity")
+	private Double bidQuantity;
+	@Column(name = "askQuantity")
+	private Double askQuantity;
+	@Column(name = "bid")
+	private Double bid;
+	@Column(name = "ask")
+	private Double ask;
+	@Column(name = "benchmark")
+	private String benchmark;
+	@Column(name = "bidListDate")
+	private Timestamp bidListDate;
+	@Column(name = "commentary")
+	private String commentary;
+	@Column(name = "security")
+	private String security;
+	@Column(name = "status")
+	private String status;
+	@Column(name = "trader")
+	private String trader;
+	@Column(name = "book")
+	private String book;
+	@Column(name = "creationName")
+	private String creationName;
+	@Column(name = "creationName")
+	private Timestamp creationDate;
+	@Column(name = "revisionName")
+	private String revisionName;
+	@Column(name = "revisionDate")
+	private Timestamp revisionDate;
+	@Column(name = "dealName")
+	private String dealName;
+	@Column(name = "dealType")
+	private String dealType;
+	@Column(name = "sourceListId")
+	private String sourceListId;
+	@Column(name = "side")
+	private String side;
+
+	public BidList(String accountTest, String typeTest, double v) {
+	}
+
+	// TODO: Map columns in data table BIDLIST with corresponding java fields
 }
