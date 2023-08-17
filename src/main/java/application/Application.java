4ea9b211a8a41e7760721c643b4d30b0cde21c5e
Mickael Haye <mickael.haye@gmail.com>
2023-08-17 16:34:17 +0200

    deplacement application méthode main, pas normal

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,9 +1,10 @@
-package com.nnk.springboot;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class Application {
 
 	public static void main(String[] args) {
