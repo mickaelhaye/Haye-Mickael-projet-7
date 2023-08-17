bf8112f86a50d2e57c486915e3608ee6f09db93b
Mickael Haye <mickael.haye@gmail.com>
2023-08-17 14:45:38 +0200

    annulation modification

--- src/main/java/com/nnk/springboot/Application.java
+++ src/main/java/com/nnk/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.nnk.springboot.domain;
+package com.nnk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
