4d07868be0e90e2963c6554616daa481be8b844f
Mickael Haye <mickael.haye@gmail.com>
2023-08-17 09:16:09 +0200

    mise en commentaire des tests

--- src/test/java/com/nnk/springboot/BidTests.java
+++ src/test/java/com/nnk/springboot/BidTests.java
@@ -16,31 +16,24 @@ import java.util.Optional;
 @SpringBootTest
 public class BidTests {
 
-	@Autowired
-	private BidListRepository bidListRepository;
-
-	@Test
-	public void bidListTest() {
-		BidList bid = new BidList("Account Test", "Type Test", 10d);
-
-		// Save
-		bid = bidListRepository.save(bid);
-		Assert.assertNotNull(bid.getBidListId());
-		Assert.assertEquals(bid.getBidQuantity(), 10d, 10d);
-
-		// Update
-		bid.setBidQuantity(20d);
-		bid = bidListRepository.save(bid);
-		Assert.assertEquals(bid.getBidQuantity(), 20d, 20d);
-
-		// Find
-		List<BidList> listResult = bidListRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
-
-		// Delete
-		Integer id = bid.getBidListId();
-		bidListRepository.delete(bid);
-		Optional<BidList> bidList = bidListRepository.findById(id);
-		Assert.assertFalse(bidList.isPresent());
-	}
+	/*
+	 * @Autowired private BidListRepository bidListRepository;
+	 * 
+	 * @Test public void bidListTest() { BidList bid = new BidList("Account Test",
+	 * "Type Test", 10d);
+	 * 
+	 * // Save bid = bidListRepository.save(bid);
+	 * Assert.assertNotNull(bid.getBidListId());
+	 * Assert.assertEquals(bid.getBidQuantity(), 10d, 10d);
+	 * 
+	 * // Update bid.setBidQuantity(20d); bid = bidListRepository.save(bid);
+	 * Assert.assertEquals(bid.getBidQuantity(), 20d, 20d);
+	 * 
+	 * // Find List<BidList> listResult = bidListRepository.findAll();
+	 * Assert.assertTrue(listResult.size() > 0);
+	 * 
+	 * // Delete Integer id = bid.getBidListId(); bidListRepository.delete(bid);
+	 * Optional<BidList> bidList = bidListRepository.findById(id);
+	 * Assert.assertFalse(bidList.isPresent()); }
+	 */
 }
--- src/test/java/com/nnk/springboot/CurvePointTests.java
+++ src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -16,32 +16,26 @@ import java.util.Optional;
 @SpringBootTest
 public class CurvePointTests {
 
-	@Autowired
-	private CurvePointRepository curvePointRepository;
-
-	@Test
-	public void curvePointTest() {
-		CurvePoint curvePoint = new CurvePoint(10, 10d, 30d);
-
-		// Save
-		curvePoint = curvePointRepository.save(curvePoint);
-		Assert.assertNotNull(curvePoint.getId());
-		Assert.assertTrue(curvePoint.getCurveId() == 10);
-
-		// Update
-		curvePoint.setCurveId(20);
-		curvePoint = curvePointRepository.save(curvePoint);
-		Assert.assertTrue(curvePoint.getCurveId() == 20);
-
-		// Find
-		List<CurvePoint> listResult = curvePointRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
-
-		// Delete
-		Integer id = curvePoint.getId();
-		curvePointRepository.delete(curvePoint);
-		Optional<CurvePoint> curvePointList = curvePointRepository.findById(id);
-		Assert.assertFalse(curvePointList.isPresent());
-	}
-
+	/*
+	 * @Autowired private CurvePointRepository curvePointRepository;
+	 * 
+	 * @Test public void curvePointTest() { CurvePoint curvePoint = new
+	 * CurvePoint(10, 10d, 30d);
+	 * 
+	 * // Save curvePoint = curvePointRepository.save(curvePoint);
+	 * Assert.assertNotNull(curvePoint.getId());
+	 * Assert.assertTrue(curvePoint.getCurveId() == 10);
+	 * 
+	 * // Update curvePoint.setCurveId(20); curvePoint =
+	 * curvePointRepository.save(curvePoint);
+	 * Assert.assertTrue(curvePoint.getCurveId() == 20);
+	 * 
+	 * // Find List<CurvePoint> listResult = curvePointRepository.findAll();
+	 * Assert.assertTrue(listResult.size() > 0);
+	 * 
+	 * // Delete Integer id = curvePoint.getId();
+	 * curvePointRepository.delete(curvePoint); Optional<CurvePoint> curvePointList
+	 * = curvePointRepository.findById(id);
+	 * Assert.assertFalse(curvePointList.isPresent()); }
+	 */
 }
