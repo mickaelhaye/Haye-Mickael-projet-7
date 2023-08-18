f2b2aa8ad7a7c4fd3ca660f41a16d5a6381d84f3
Mickael Haye <mickael.haye@gmail.com>
2023-08-18 11:55:58 +0200

    security configuration

---
+++ src/main/java/com/nnk/springboot/configuration/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.nnk.springboot.configuration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		//@formatter:off
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                        		.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        		.requestMatchers("/*").permitAll()
+                        		.requestMatchers("/bidList/*").permitAll()
+                        		.requestMatchers("/bidList/update/{id}").permitAll()
+                        		.requestMatchers("/bidList/delete/{id}").permitAll()
+                        		                           
+                )
+                .formLogin(withDefaults())
+                .rememberMe(withDefaults());
+               
+ 					
+                
+		return http.build();
+		//@formatter:on
+
+	}
+
+}
