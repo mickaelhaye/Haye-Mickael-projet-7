264226e66bc11a6a76f5ec787f4735d6b1a16338
Mickael Haye <mickael.haye@gmail.com>
2023-08-23 16:15:53 +0200

    gatsion login et logout

--- src/main/java/com/nnk/springboot/configuration/SecurityConfiguration.java
+++ src/main/java/com/nnk/springboot/configuration/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.nnk.springboot.configuration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,31 +15,39 @@ public class SecurityConfiguration {
 	//@formatter:off
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-				 .requestMatchers("/bidList/*").authenticated()
-				 .requestMatchers("/bidList/update/{id}").authenticated()
-				 .requestMatchers("/bidList/delete/{id}").authenticated()
-				 .requestMatchers("/curvePoint/*").authenticated()
-				 .requestMatchers("/curvePoint/update/{id}").authenticated()
-				 .requestMatchers("/curvePoint/delete/{id}").authenticated()
-				 .requestMatchers("/rating/*").authenticated()
-				 .requestMatchers("/rating/update/{id}").authenticated()
-				 .requestMatchers("/rating/delete/{id}").authenticated()
-				 .requestMatchers("/ruleName/*").authenticated()
-				 .requestMatchers("/ruleName/update/{id}").authenticated()
-				 .requestMatchers("/ruleName/delete/{id}").authenticated()
-				 .requestMatchers("/trade/*").authenticated()
-				 .requestMatchers("/trade/update/{id}").authenticated()
-				 .requestMatchers("/trade/delete/{id}").authenticated()
-				 .requestMatchers("/user/*").permitAll()
-				 .requestMatchers("/user/update/{id}").authenticated()
-				 .requestMatchers("/user/delete/{id}").authenticated()
-		    	 .requestMatchers("/admin/home").hasRole("ADMIN")
-		    	 .anyRequest().permitAll()
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers("/bidList/*").authenticated()
+                .requestMatchers("/bidList/update/{id}").authenticated()
+                .requestMatchers("/bidList/delete/{id}").authenticated()
+                .requestMatchers("/curvePoint/*").authenticated()
+                .requestMatchers("/curvePoint/update/{id}").authenticated()
+                .requestMatchers("/curvePoint/delete/{id}").authenticated()
+                .requestMatchers("/rating/*").authenticated()
+                .requestMatchers("/rating/update/{id}").authenticated()
+                .requestMatchers("/rating/delete/{id}").authenticated()
+                .requestMatchers("/ruleName/*").authenticated()
+                .requestMatchers("/ruleName/update/{id}").authenticated()
+                .requestMatchers("/ruleName/delete/{id}").authenticated()
+                .requestMatchers("/trade/*").authenticated()
+                .requestMatchers("/trade/update/{id}").authenticated()
+                .requestMatchers("/trade/delete/{id}").authenticated()
+                .requestMatchers("/user/*").permitAll()
+                .requestMatchers("/user/update/{id}").authenticated()
+                .requestMatchers("/user/delete/{id}").authenticated()
+                .requestMatchers("/admin/home").hasRole("ADMIN")
+                .anyRequest().permitAll())
 
-		)
-		.formLogin(withDefaults())
-		.exceptionHandling(handling -> handling.accessDeniedPage("/app/error"));
+                .formLogin(login -> login.permitAll()
+                        .loginPage("/app/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/app/login_success")
+                        .failureUrl("/app/login_error"))
+                .logout(logout -> logout.permitAll()
+                		.logoutUrl("/app-logout")
+                		.logoutSuccessUrl("/logout_success")
+                		.deleteCookies("remove"))
+                .exceptionHandling(handling -> handling.accessDeniedPage("/app/error"));
 
 		return http.build();
 		//@formatter:on
