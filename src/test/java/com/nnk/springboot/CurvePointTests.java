0ed55674be330400dd78d443c3d366f29889f94b
Mickael Haye <mickael.haye@gmail.com>
2023-08-25 11:40:28 +0200

    test CurvePoint

---
+++ src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -0,0 +1,60 @@
+package com.nnk.springboot;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+
+/**
+ * this class is to test for the entity CurvePoint.
+ * 
+ * @author mickael hayé
+ * @version 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurvePointTests {
+
+	@Autowired
+	private CurvePointRepository curvePointRepository;
+
+	@Test
+	public void curvePointTest() {
+		CurvePoint curvePoint = new CurvePoint();
+		curvePoint.setCurve_id(10);
+		curvePoint.setTerm(10d);
+		curvePoint.setValue(30d);
+
+		// Save
+		curvePoint = curvePointRepository.save(curvePoint);
+		assertNotNull(curvePoint.getId());
+		assertTrue(curvePoint.getCurve_id() == 10);
+
+		// Update
+		curvePoint.setCurve_id(20);
+		curvePoint = curvePointRepository.save(curvePoint);
+		assertTrue(curvePoint.getCurve_id() == 20);
+
+		// Find
+		List<CurvePoint> listResult = curvePointRepository.findAll();
+		assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = curvePoint.getId();
+		curvePointRepository.delete(curvePoint);
+		Optional<CurvePoint> curvePointList = curvePointRepository.findById(id);
+		assertFalse(curvePointList.isPresent());
+	}
+
+}
