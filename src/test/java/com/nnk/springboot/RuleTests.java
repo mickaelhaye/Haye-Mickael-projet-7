f306871035011c8e20f064e9ec4aabd6eaa33915
Mickael Haye <mickael.haye@gmail.com>
2023-08-25 13:02:07 +0200

    Rule test

--- src/test/java/com/nnk/springboot/RuleTests.java
+++ src/test/java/com/nnk/springboot/RuleTests.java
@@ -1,6 +1,19 @@
 package com.nnk.springboot;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.services.RuleNameService;
 
 /**
  * this class is to test for the entity RuleName.
@@ -8,28 +21,45 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @author mickael hayé
  * @version 1.0
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class RuleTests {
 
-	/*
-	 * @Autowired private RuleNameRepository ruleNameRepository;
-	 * 
-	 * @Test public void ruleTest() { RuleName rule = new RuleName("Rule Name",
-	 * "Description", "Json", "Template", "SQL", "SQL Part");
-	 * 
-	 * // Save rule = ruleNameRepository.save(rule);
-	 * Assert.assertNotNull(rule.getId());
-	 * Assert.assertTrue(rule.getName().equals("Rule Name"));
-	 * 
-	 * // Update rule.setName("Rule Name Update"); rule =
-	 * ruleNameRepository.save(rule);
-	 * Assert.assertTrue(rule.getName().equals("Rule Name Update"));
-	 * 
-	 * // Find List<RuleName> listResult = ruleNameRepository.findAll();
-	 * Assert.assertTrue(listResult.size() > 0);
-	 * 
-	 * // Delete Integer id = rule.getId(); ruleNameRepository.delete(rule);
-	 * Optional<RuleName> ruleList = ruleNameRepository.findById(id);
-	 * Assert.assertFalse(ruleList.isPresent()); }
-	 */
+	@Autowired
+	private RuleNameService ruleNameService;
+
+	@Test
+	public void ruleTest() {
+		RuleName rule = new RuleName();
+		rule.setName("Rule Name");
+		rule.setDescription("Description");
+		rule.setJson("Json");
+		rule.setTemplate("Template");
+		rule.setSql("SQL");
+		rule.setSqlPart("SQL Part");
+
+		// Save
+		rule = ruleNameService.addRuleName(rule);
+		assertNotNull(rule.getId());
+		assertTrue(rule.getName().equals("Rule Name"));
+
+		// Update
+		rule.setName("Rule Name Update");
+		rule = ruleNameService.addRuleName(rule);
+		assertTrue(rule.getName().equals("Rule Name Update"));
+
+		// Find
+		List<RuleName> listResult = (List<RuleName>) ruleNameService.getRuleNames();
+		assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = rule.getId();
+		ruleNameService.delRuleName(rule);
+		RuleName ruleList = null;
+		try {
+			ruleList = ruleNameService.getRuleNameById(id);
+		} catch (Exception e) {
+			assertNull(ruleList);
+		}
+	}
 }
